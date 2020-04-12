d794f5637fa6da0f07b85786dbb595a755908ca7
Charlie Payne <charliepayne1101@gmail.com>
2020-04-12 08:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+function takeANumber(katzDeliLine) {
+  var number = katzDeliLine.length + 1;
+  return `Welcome, Your number is .${number}`;
 }
 
 function nowServing(katzDeliLine) {
@@ -8,16 +8,15 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!";
   }
   else {return "Currently serving " + katzDeliLine.shift() + ".";
-}
+  }
 }
 
 function currentLine(katzDeliLine) {
   var line = [];
-  var i = 0
   if (katzDeliLine.length < 1) {
     return "The line is currently empty.";
   }
-  else { for (i = 0; i < katzDeliLine.length; i++) {line.push( ` ` + [i + 1] + `. ` + katzDeliLine[i] )}
-  return (`The line is currently:` + line)
-}
+  else {for (let i = 0; i < katzDeliLine.length; i++) {line.push( ` ` + [i + 1] + `. ` + katzDeliLine[i] )}
+  return (`The line is currently:` + line);
+  }
 }
